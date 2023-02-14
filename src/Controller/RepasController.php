f9341c7c8edbdc886ba46d2253da78fc21f24b1f
Josic Libert <69466841+Zaratchy@users.noreply.github.com>
2023-02-14 16:28:38 +0100

    feat(restaurant): add show repas and created repas

---
+++ src/Controller/RepasController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\RepasType;
+use App\Entity\Repas;
+use App\Repository\RepasRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class RepasController extends AbstractController
+{
+    /**
+     * @Route("/repas", name="repas")
+     */
+    public function index(RepasRepository $repasRepository): Response
+    {
+        $repas = $repasRepository->findAll();
+        return $this->render('repas/index.html.twig', [
+            'repass' => $repas,
+        ]);
+    }
+
+    /**
+     * @Route("/repas/new", name="new-repas", methods={"GET", "POST"})
+     */
+    public function new(Request $request, RepasRepository $repasRepository, ManagerRegistry $doctrine): Response
+    {
+        $repas = new Repas();
+        $form = $this->createForm(RepasType::class, $repas);
+        
+        $manager = $doctrine->getManager();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repasRepository->add($repas, true);
+            $repas = $form->getData();
+            $manager->persist($repas);
+            $manager->flush();
+
+            return $this->redirectToRoute('repas', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('repas/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
