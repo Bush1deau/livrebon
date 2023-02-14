436bb65a9e491d6d43e97dd164e9a460529ab340
Julian <juju.leroy47@gmail.com>
2023-02-14 10:05:59 +0100

    add feature connect and  hash password

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,13 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      */
-    public function register(Request $request, EntityManagerInterface $manager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $manager): Response
     {
         $user = new User();
         $form=$this->createForm(RegistrationType::class, $user);
@@ -23,6 +24,12 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
 
             if ($form->get('typeCompte')->getData()) {
                 $user->setRoles([$form->get('typeCompte')->getData()]);
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -68,16 +68,21 @@ class RegistrationType extends AbstractType
 
 
             ->add('password', PasswordType::class, [
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'label' => 'Mot de passe',
-                'label_attr' => [
-                    'class' => 'form-label mt-4' 
-                ],
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new Assert\NotBlank()
-                ]
+                    new Assert\NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Assert\Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
             ])
 
 
